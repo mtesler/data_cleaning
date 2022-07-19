a499fa0b1b47847812102e64436b2f22fce420e9
Michal Tesler <michal.tesler@gmail.com>
2022-07-19 07:51:16 +0200

    Added new column to a dataframe with extracted minutes from duration column

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,9 +1,10 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # dataset overview
 df_netflix = pd.read_csv('netflix_titles.csv')
-print(df_netflix.dtypes)
-print(df_netflix.shape)
+# print(df_netflix.dtypes)
+# print(df_netflix.shape)
 
 # identify missing data
 print(df_netflix.isnull().sum())
@@ -12,3 +13,8 @@ print(df_netflix.isnull().sum())
 for column in df_netflix.columns:
     percentage = df_netflix[column].isnull().mean()
     print(f'{column}: {round(percentage*100, 2)}%')
+
+# use histograms to identify outliers with numeric data
+df_movie = df_netflix[df_netflix['type'] == 'Movie']
+df_movie = df_movie.assign(minute=df_movie['duration'].str.extract(
+    '(\d+)', expand=False))
